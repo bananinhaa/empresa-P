7a31d7d9b4e7c773adfa27935149cb7b034761ab
bananinhaa <rafa.oliveira2003@gmail.com>
2020-08-15 18:32:59 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function calculo(){
+    var er1 = document.getElementById("di1");
+
+    let nome = document.getElementById("nome").value;
+    let quanthr = document.getElementById("quanthr").value;
+    let valorhr = document.getElementById("valorhr").value;
+    let imposto = document.getElementById("imposto").value;
+
+    // calculo do salario bruto
+    let salaBruto = quanthr*valorhr;
+
+    // calculo do salario liquido 
+    let salaLiquido = salaBruto-imposto;
+
+    // print array
+    er1.innerText = "Salario bruto: " + salaBruto + "\nSalario liquido: " + salaLiquido;
+}
+
+function limpar() {
+    var nome = document.getElementById("nome");
+    var valor = document.getElementById("quanthr");
+    var horas = document.getElementById("valorhr");
+    var imposto = document.getElementById("imposto");
+
+    nome.value = "";
+    valor.value = "";
+    horas.value = "";
+
+    console.clear();
+}
